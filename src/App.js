96a94ae09c876dce96d7e4cce7849d5e13188af0
coenaraya <javier.araya.h@gmail.com>
2022-08-23 23:48:07 -0300

    primer landing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       
         < Navbar />
-        <ItemListContainer titular="Bienvenido a la tienda" nombre ="Joaco" />
+        <ItemListContainer titular="Bienvenido a la tienda" nombre ="usuario" />
         < Maincont/>
         < Footer />
     </div>
