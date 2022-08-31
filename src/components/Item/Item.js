0d9602b601daa4349933560fe23c17161bd5d78b
coenaraya <javier.araya.h@gmail.com>
2022-08-31 20:11:09 -0300

    desafio maps y promises

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,24 @@
 import '../ItemList/item.scss';
 import {Contador} from '../ItemCount/ItemCount';
+import { useState } from "react";
 
 const Item = ( {producto} ) => {
+
+    const {stock} = producto
+
+    const [counter, setCounter] = useState(1)
+
+    
+    const handleSumar = () => {
+        if(counter < stock)
+        setCounter(counter + 1)
+    }
+
+    const handleRestar = () => {
+        if (counter > 1 ) {
+            setCounter(counter - 1)
+        }
+    }
 
     return (
         <div className="imgCard">
@@ -11,7 +28,7 @@ const Item = ( {producto} ) => {
             <p>Precio: {producto.precio}</p>
             <small>Stock disponible: {producto.stock}</small>
             <p>{producto.desc}</p>
-            <Contador stock= {producto.stock}/>
+            <Contador onAdd= {handleSumar} counter= {counter} onDecrement= {handleRestar}/>
             
             
         </div>
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,31 +1,14 @@
-import { useState } from "react"
-
-
 
 export const Contador = (producto) => {
 
-    const {stock} = producto
+    const {counter, onAdd, onDecrement} = producto
     
-    const [counter, setCounter] = useState(1)
-
     
-    const handleSumar = () => {
-        if(counter < stock)
-        setCounter(counter + 1)
-    }
-
-    const handleRestar = () => {
-        if (counter > 1 ) {
-            setCounter(counter - 1)
-        }
-    }
-
-
     return (
         <div className="container">
-            <button onClick={handleRestar} className="btn btn-outline-primary">-</button>
+            <button onClick={onDecrement} className="btn btn-outline-primary">-</button>
             <span className="mx-2">{counter}</span>
-            <button onClick={handleSumar} className="btn btn-primary">+</button>
+            <button onClick={onAdd} className="btn btn-primary">+</button>
             <hr/>
             <button className="btn btn-success">Agregar al carrito</button>
         </div>
