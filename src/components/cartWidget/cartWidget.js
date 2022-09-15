f66eb3e0e84eab941e0b5f56a2eff8efdc6b766f
CoenAraya <102705035+CoenAraya@users.noreply.github.com>
2022-09-15 00:05:29 -0300

    entrega cart

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -2,12 +2,14 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {CartContext} from '../../context/CartContext'
+import "./cartWidget.scss";
+
 
 export const CartWidget = () => {
     const {cartCant} = useContext(CartContext)
     return(
-        <Link to="/cart">
-            <ShoppingCartIcon sx={{ fontSize: 25 }}/>
+        <Link to="/cart" className={cartCant() > 0 ? "cartView" : "cartHide"}>
+            <ShoppingCartIcon sx={{ fontSize: 25 }} />
             <span>{cartCant()}</span>
         </Link>
     )
